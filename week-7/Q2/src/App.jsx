491bc3cd0df18d37ec1dd81a91411a9a8e56b368
Shruti Singh <singh001shruti@gmail.com>
2024-01-16 22:34:52 +0530

    Q2 done

---
+++ week-7/Q2/src/App.jsx
@@ -0,0 +1,12 @@
+import Colour_Change from "../src/Components/Background_Change/Colour_Change"
+
+function App() {
+
+  return (
+    <>
+      <Colour_Change />
+    </>
+  )
+}
+
+export default App
